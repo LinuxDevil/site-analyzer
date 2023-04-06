daec6f86265ffce9462f4f46a32633e760adc089
Ali Mohammad <alimohammad@pc-2473.home>
2023-04-06 23:59:19 +0300

    fix: ignore the use of the location

--- client/src/data/Axios.ts
+++ client/src/data/Axios.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
